0de587a155cae2db52ecc0f5a6e2ca4a694fa048
yidafu <me@yidefu.dev>
2024-11-22 00:28:12 +0800

    feat: i18n handler

---
+++ server/src/main/kotlin/dev/yidafu/blog/handler/CommonHandler.kt
@@ -0,0 +1,56 @@
+package dev.yidafu.blog.handler
+
+import de.comahe.i18n4k.Locale
+import dev.yidafu.blog.common.ConfigurationKeys
+import dev.yidafu.blog.common.ConstantKeys
+import dev.yidafu.blog.bean.bo.ConfigurationBO
+import dev.yidafu.blog.service.ConfigurationService
+import io.vertx.core.http.Cookie
+import io.vertx.core.http.HttpHeaders
+import io.vertx.ext.web.RoutingContext
+import org.koin.core.annotation.Single
+import org.slf4j.LoggerFactory
+
+@Single
+class CommonHandler(
+  private val configurationService: ConfigurationService,
+) {
+  private val log = LoggerFactory.getLogger(CommonHandler::class.java)
+  suspend fun initConfiguration(ctx: RoutingContext) {
+    val configs = configurationService.getAll()
+    val siteTitle = configs.find { it.configKey == ConfigurationKeys.SITE_TITLE }?.configValue ?: ""
+    val githubUrl = configs.find { it.configKey == ConfigurationKeys.GITHUB_URL }?.configValue ?: ""
+
+    val bo = ConfigurationBO(siteTitle, githubUrl)
+    log.info("config bo $bo")
+    ctx.put(ConstantKeys.GLOBAL_CONFIGURATION, bo)
+    ctx.next()
+  }
+
+  fun localHandler(ctx: RoutingContext) {
+    val language = ctx.request().getParam("lang")
+
+    if (!language.isNullOrBlank()) {
+      ctx.response().addCookie(Cookie.cookie(ConstantKeys.LANGUAGE_COOKIE_KEY, language).setHttpOnly(true))
+      val uri = ctx.request().uri().replace("lang=$language", "").removeSuffix("?")
+      ctx.redirect(uri)
+      return;
+    }
+
+    val cookieLang = ctx.request().cookies(ConstantKeys.LANGUAGE_COOKIE_KEY)
+
+    val cLang = if (cookieLang.isEmpty()) {
+      ctx.request().getHeader(HttpHeaders.ACCEPT_LANGUAGE)
+    } else {
+      cookieLang.first().value
+    }
+    val finalLanguage = cLang ?: ConstantKeys.DEFAULT_LANGUAGE
+    log.info("current language $finalLanguage")
+    ctx.put(ConstantKeys.LANGUAGE_CONTEXT, Locale.forLanguageTag(finalLanguage))
+    ctx.next()
+  }
+
+  companion object {
+    const val GLOBAL_CONFIGURATION = "global_configuration"
+  }
+}
--- server/src/main/kotlin/dev/yidafu/blog/routes/PostRoutes.kt
+++ server/src/main/kotlin/dev/yidafu/blog/routes/PostRoutes.kt
@@ -2,6 +2,8 @@ package dev.yidafu.blog.routes
 
 import dev.yidafu.blog.handler.HomePageHandler
 import io.vertx.ext.web.Router
+import io.vertx.ext.web.handler.LoggerHandler
+import io.vertx.ext.web.handler.StaticHandler
 import io.vertx.kotlin.coroutines.CoroutineRouterSupport
 import org.koin.core.Koin
 import org.koin.core.KoinApplication
@@ -10,7 +12,7 @@ import org.koin.core.KoinApplication
 fun CoroutineRouterSupport.mountPostRoutes(router: Router, koin: Koin) {
 
   val homeHandler = koin.get<HomePageHandler>()
-
+  router.route("/public/*").handler(StaticHandler.create("public"))
   router.get("/")
     .coHandler(requestHandler =  homeHandler::indexPage)
 
