b414f2e652d8366ab4cd980a347e8a8491e2b081
yidafu <tiaozuwushi@qq.com>
2025-01-20 00:07:50 +0800

    refactor(server): new koin DI implement of GitSynchronousTask

--- server/src/main/kotlin/dev/yidafu/blog/MainVerticle.kt
+++ server/src/main/kotlin/dev/yidafu/blog/MainVerticle.kt
@@ -2,11 +2,13 @@ package dev.yidafu.blog
 
 import dev.yidafu.blog.admin.AdminVerticle
 import dev.yidafu.blog.admin.handler.AdminHandlerModule
+import dev.yidafu.blog.admin.jobs.DBArticleManager
+import dev.yidafu.blog.admin.jobs.DBLogger
 import dev.yidafu.blog.admin.services.AdminServiceModule
 import dev.yidafu.blog.common.dao.DefaultSchema
-import dev.yidafu.blog.common.dao.tables.BArticle
 import dev.yidafu.blog.common.handler.CommonHandlerModule
 import dev.yidafu.blog.common.services.CommonServiceModule
+import dev.yidafu.blog.dev.yidafu.blog.engine.*
 import dev.yidafu.blog.fe.FrontendVerticle
 import dev.yidafu.blog.fe.handler.FeHandlerModule
 import dev.yidafu.blog.fe.service.FeServiceModule
@@ -15,7 +17,6 @@ import io.vertx.kotlin.coroutines.CoroutineVerticle
 import org.jooq.CloseableDSLContext
 import org.jooq.DDLExportConfiguration
 import org.jooq.DDLFlag
-import org.jooq.DSLContext
 import org.jooq.impl.DSL
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
@@ -34,20 +35,20 @@ class MainVerticle : CoroutineVerticle(), CoroutineRouterSupport {
   }
 
   override suspend fun start() {
-
-    val jooqContext: CloseableDSLContext = DSL.using(
-      "jdbc:sqlite:./nice-blog.db",
-      "",
-      ""
-    )
+    val jooqContext: CloseableDSLContext =
+      DSL.using(
+        "jdbc:sqlite:./nice-blog.db",
+        "",
+        "",
+      )
     dslContext = jooqContext
 
-    val dbConfig = DDLExportConfiguration()
-      .flags(DDLFlag.TABLE, DDLFlag.PRIMARY_KEY, DDLFlag.UNIQUE, DDLFlag.INDEX, DDLFlag.COMMENT)
-      .createTableIfNotExists(true)
-      .createSchemaIfNotExists(true)
-      .createSequenceIfNotExists(true)
-
+    val dbConfig =
+      DDLExportConfiguration()
+        .flags(DDLFlag.TABLE, DDLFlag.PRIMARY_KEY, DDLFlag.UNIQUE, DDLFlag.INDEX, DDLFlag.COMMENT)
+        .createTableIfNotExists(true)
+        .createSchemaIfNotExists(true)
+        .createSequenceIfNotExists(true)
 
     jooqContext.ddl(DefaultSchema.DEFAULT_SCHEMA, dbConfig)
       .queries()
@@ -59,25 +60,38 @@ class MainVerticle : CoroutineVerticle(), CoroutineRouterSupport {
       .filterNot { it.startsWith("--") }
       .forEach { jooqContext.execute(it) }
 
-    val koin = startKoin {
-      printLogger()
+    val koin =
+      startKoin {
+        printLogger()
 
-      val JooqModule = module {
-        factory<CloseableDSLContext> { jooqContext }
+        val JooqModule =
+          module {
+            factory<CloseableDSLContext> { jooqContext }
+          }
+        val engineModule = module {
+          single<Logger> { DBLogger("xx") }
+          single<GitSynchronousTaskTemplate> {
+            GitSynchronousTask(
+              GitConfig("https://github.com/yidafu/example-blog.git", branch = "master"),
+              DefaultSynchronousListener()
+            )
+          }
+          single<ArticleManager> {
+            DBArticleManager()
+          }
+        }
+
+        modules(
+          JooqModule,
+          engineModule,
+          CommonHandlerModule().module,
+          CommonServiceModule().module,
+          FeServiceModule().module,
+          FeHandlerModule().module,
+          AdminHandlerModule().module,
+          AdminServiceModule().module,
+        )
       }
-      modules(
-        JooqModule,
-        CommonHandlerModule().module,
-        CommonServiceModule().module,
-
-        FeServiceModule().module,
-        FeHandlerModule().module,
-
-        AdminHandlerModule().module,
-        AdminServiceModule().module,
-      )
-    }
-
 
     log.info("start FrontendVerticle")
     vertx.deployVerticle(FrontendVerticle(koin.koin)).andThen { res ->
@@ -88,7 +102,6 @@ class MainVerticle : CoroutineVerticle(), CoroutineRouterSupport {
     log.info("start AdminVerticle")
     vertx.deployVerticle(AdminVerticle(koin.koin)).andThen { res ->
       developmentIdList.add(res.result())
-
     }
   }
 
--- server/src/main/kotlin/dev/yidafu/blog/admin/handler/SynchronousHandler.kt
+++ server/src/main/kotlin/dev/yidafu/blog/admin/handler/SynchronousHandler.kt
@@ -1,40 +1,30 @@
 package dev.yidafu.blog.admin.handler
 
-import dev.yidafu.blog.admin.jobs.NiceGitSynchronousTask
 import dev.yidafu.blog.admin.services.SyncTaskService
 import dev.yidafu.blog.admin.views.pages.sync.AdminSyncLogPage
 import dev.yidafu.blog.admin.views.pages.sync.AdminSyncOperatePage
-import dev.yidafu.blog.common.BlogConfig
 import dev.yidafu.blog.common.Routes
 import dev.yidafu.blog.common.converter.SyncTaskConvertor
-import dev.yidafu.blog.common.dto.MarkdownArticleDTO
 import dev.yidafu.blog.common.ext.html
-import dev.yidafu.blog.common.modal.ArticleModel
-import dev.yidafu.blog.common.modal.ArticleStatus
 import dev.yidafu.blog.common.modal.SyncTaskStatus
 import dev.yidafu.blog.common.query.PageQuery
 import dev.yidafu.blog.common.services.ArticleService
 import dev.yidafu.blog.common.vo.AdminSyncTaskListVO
 import dev.yidafu.blog.common.vo.AdminSynchronousVO
-import dev.yidafu.blog.common.vo.PageVO
-import dev.yidafu.blog.dev.yidafu.blog.engine.SyncContext
+import dev.yidafu.blog.dev.yidafu.blog.engine.GitConfig
+import dev.yidafu.blog.dev.yidafu.blog.engine.GitSynchronousTaskTemplate
 import io.github.allangomes.kotlinwind.css.I300
 import io.github.allangomes.kotlinwind.css.I50
 import io.github.allangomes.kotlinwind.css.LG
 import io.github.allangomes.kotlinwind.css.kw
 import io.vertx.ext.web.RoutingContext
-import io.vertx.kotlin.coroutines.vertxFuture
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.html.div
-import kotlinx.html.pre
 import kotlinx.html.stream.appendHTML
 import kotlinx.html.style
 import org.koin.core.annotation.Single
+import org.koin.java.KoinJavaComponent.inject
 import org.mapstruct.factory.Mappers
-import java.io.File
-import java.nio.file.Files
-import java.nio.file.Paths
 import java.util.*
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
@@ -48,6 +38,9 @@ class SynchronousHandler(
   private val LOG_END_EVENT = "logEnd"
 
   private val syncTaskConvertor = Mappers.getMapper(SyncTaskConvertor::class.java)
+
+  private val syncTask: GitSynchronousTaskTemplate by inject(GitSynchronousTaskTemplate::class.java)
+
   suspend fun syncPage(ctx: RoutingContext) {
     ctx.redirect(Routes.SYNC_OPERATE_URL)
   }
@@ -63,7 +56,6 @@ class SynchronousHandler(
   }
 
   suspend fun syncOperatePage(ctx: RoutingContext) {
-
     ctx.html(AdminSyncOperatePage::class, AdminSynchronousVO(""))
   }
 
@@ -72,32 +64,36 @@ class SynchronousHandler(
     val taskUuid = Uuid.random().toHexString()
 
     syncTaskService.createSyncTask(taskUuid)
-    val syncCtx = DbSyncContext(
-      taskUuid,
-      syncTaskService,
-      SyncContext.GitConfig("https://github.com/yidafu/example-blog.git", branch = "master")
-    )
-    val htmlFragment = buildString {
-      appendHTML().div {
-        style = kw.inline { background.gray[I50]; padding[4]; border.rounded[LG]; border.gray[I300] }
-        attributes["hx-ext"] = "sse"
-        attributes["sse-connect"] = Routes.SYNC_API_LOG_URL.replace(":uuid", taskUuid)
-        attributes["sse-swap"] = "message"
-        attributes["sse-close"] = LOG_END_EVENT
+    val syncCtx = GitConfig("https://github.com/yidafu/example-blog.git", branch = "master")
 
-        div {
-          attributes["sse-swap"] = LOG_APPEND_EVENT
-          attributes["hx-swap"] = "beforeend"
-        }
-        div {
-          attributes["sse-swap"] = LOG_END_EVENT
+    val htmlFragment =
+      buildString {
+        appendHTML().div {
+          style =
+            kw.inline {
+              background.gray[I50]
+              padding[4]
+              border.rounded[LG]
+              border.gray[I300]
+            }
+          attributes["hx-ext"] = "sse"
+          attributes["sse-connect"] = Routes.SYNC_API_LOG_URL.replace(":uuid", taskUuid)
+          attributes["sse-swap"] = "message"
+          attributes["sse-close"] = LOG_END_EVENT
+
+          div {
+            attributes["sse-swap"] = LOG_APPEND_EVENT
+            attributes["hx-swap"] = "beforeend"
+          }
+          div {
+            attributes["sse-swap"] = LOG_END_EVENT
+          }
         }
       }
-    }
     ctx.end(htmlFragment)
     // async execute task
     withContext(Dispatchers.IO) {
-      NiceGitSynchronousTask(syncCtx, DbDelegate(syncCtx, articleService)).sync()
+      syncTask.sync()
     }
   }
 
@@ -125,21 +121,23 @@ class SynchronousHandler(
               SseModel(
                 data = str,
                 event = LOG_APPEND_EVENT,
-              ).toString()
+              ).toString(),
             )
           }
           response.end(
             SseModel(
               data = "=== LOG END ===",
               event = LOG_END_EVENT,
-            ).toString()
+            ).toString(),
           )
           return
         } else {
           val currentLog = log.logs
           if (currentLog != null) {
             val appendText = if (previewLog.isEmpty()) currentLog else currentLog.substring(previewLog.length)
-            println("${previewLog.isEmpty()}   current log ${currentLog.length},  preview log ${previewLog.length},  appendText ${appendText.length}")
+            println(
+              "${previewLog.isEmpty()}   current log ${currentLog.length},  preview log ${previewLog.length},  appendText ${appendText.length}",
+            )
             previewLog = currentLog
             if (appendText.isNotEmpty()) {
               appendText.split('\n').forEach { str ->
@@ -148,7 +146,7 @@ class SynchronousHandler(
                     data = str,
                     event = LOG_APPEND_EVENT,
 //                      id = pollingCount.toString()
-                  ).toString()
+                  ).toString(),
                 )
               }
             }
@@ -160,53 +158,12 @@ class SynchronousHandler(
 //    ctx.end("log $uuid")
   }
 
-  class DbDelegate(
-    private val ctx: SyncContext,
-    private val articleService: ArticleService,
-  ) : NiceGitSynchronousTask.DatabaseDelegate {
-    override fun uploadFile(file: File): String {
-      val directory = File(BlogConfig.DEFAULT_UPLOAD_DIRECTORY)
-
-      if (!directory.exists()) {
-        directory.mkdirs()
-      }
-      val newFilename = UUID.randomUUID().toString() + "." + file.extension
-      val newFilePath = Paths.get(directory.path, newFilename)
-      ctx.log("copy ${file.toPath()} to $newFilePath")
-      Files.copy(file.toPath(), newFilePath)
-      return Routes.UPLOAD_URL.replace("*", newFilename)
-    }
-
-    override fun updateArticle(dto: MarkdownArticleDTO): Boolean {
-      println(dto.filename)
-      val identifier = dto.filename.replace(".md", "")
-      val modal = ArticleModel(
-        title = dto.frontMatter?.title ?: "",
-        summary = dto.frontMatter?.description,
-        cover = dto.frontMatter?.cover,
-        identifier = identifier,
-        html = dto.html,
-        series = "",
-        content = dto.rawContext,
-        status = ArticleStatus.Candidate,
-      )
-      modal.createdAt = dto.createTime
-      modal.updatedAt = dto.updateTime
-      CoroutineScope(Dispatchers.IO).launch {
-        articleService.saveArticle(modal)
-      }
-      return true
-    }
-
-  }
-
   data class SseModel(
     val event: String? = null,
     val data: String = "",
     val id: String? = null,
     val retry: Number? = null,
   ) {
-
     override fun toString(): String {
       val sseStrings = arrayListOf<String>()
 
@@ -217,45 +174,6 @@ class SynchronousHandler(
       sseStrings.add("\n")
 
       return sseStrings.joinToString(separator = "\n")
-    }
-  }
-
-  class DbSyncContext(
-    private val uuid: String,
-    private val syncTaskService: SyncTaskService,
-    override val gitConfig: GitConfig,
-  ) : SyncContext() {
-    private val flow = MutableSharedFlow<String>(10, 10)
-
-    init {
-      CoroutineScope(Dispatchers.IO).launch {
-        // append log in order
-        flow.collect {
-          syncTaskService.appendLog(uuid, it)
-        }
-      }
-    }
-
-    override fun log(str: String) {
-      flow.tryEmit(str + "\n")
-    }
-
-    override fun onStart() {
-      CoroutineScope(Dispatchers.IO).launch {
-        syncTaskService.changeStatus(uuid, SyncTaskStatus.Running)
-      }
-    }
-
-    override fun onFinish() {
-      CoroutineScope(Dispatchers.IO).launch {
-        syncTaskService.changeStatus(uuid, SyncTaskStatus.Finished)
-      }
-    }
-
-    override fun onFailed() {
-      CoroutineScope(Dispatchers.IO).launch {
-        syncTaskService.changeStatus(uuid, SyncTaskStatus.Failed)
-      }
     }
   }
 }
